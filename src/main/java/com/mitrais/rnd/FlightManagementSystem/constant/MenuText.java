f4eb2570a6c154d3dd07341ea67b46e36da74f92
destanti-mitrais <destanti.fatwakhyuni@mitrais.com>
2025-09-10 11:46:59 +0700

    Create register_aircraft

--- src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
+++ src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
@@ -1,11 +1,17 @@
 package com.mitrais.rnd.FlightManagementSystem.constant;
 
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MenuText {
-    private static final String ADMIN_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Aircraft Management\n2.Destination Management\n3.Route Creation\n4.System Operation";
+    private static final String ADMIN_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Register Aircraft\n2.Register New Destination\n3.Create Route\n4.System Operation";
     private static final String PASSENGER_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Book Flight\n2.My Bookings";
+    private static final String SUCCESS_ADD_AIRCRAFT = "Aircraft %s with %s capacity is successfully saved!\n\n";
+
+    public static final String REGISTER_AIRCRAFT_HEADER = "---------------REGISTER AIRCRAFT---------------\n";
+    public static final String ENTER_AIRCRAFT_NAME = "Enter aircraft name: ";
+    public static final String AIRCRAFT_CAPACITY = "Enter aircraft capacity: ";
 
     public static String getAdminMenuDisplayText(String name) {
         return String.format(ADMIN_MENU_DISPLAY, name);
@@ -13,6 +19,10 @@ public class MenuText {
 
     public static String getPassengerMenuDisplayText(String name) {
         return String.format(PASSENGER_MENU_DISPLAY, name);
+    }
+
+    public static String getSuccessAddAircraft(String name, int capacity) {
+        return String.format(SUCCESS_ADD_AIRCRAFT, name, capacity);
     }
 
 }
--- src/main/java/com/mitrais/rnd/FlightManagementSystem/controller/AdminMenuDisplay.java
+++ src/main/java/com/mitrais/rnd/FlightManagementSystem/controller/AdminMenuDisplay.java
@@ -1,7 +1,7 @@
 package com.mitrais.rnd.FlightManagementSystem.controller;
 
 import com.mitrais.rnd.FlightManagementSystem.util.UserContextHolder;
-import com.mitrais.rnd.FlightManagementSystem.constant.AdminOptions;
+import com.mitrais.rnd.FlightManagementSystem.enums.AdminOptions;
 import com.mitrais.rnd.FlightManagementSystem.constant.MenuText;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,21 +12,22 @@ import java.util.Scanner;
 @RequiredArgsConstructor
 public class AdminMenuDisplay implements Displayable{
 
+    private final RegisterAircraftDisplay registerAircraftDisplayScreen;
+
     @Override
     public void display() {
         System.out.println(MenuText.getAdminMenuDisplayText(UserContextHolder.getUserContext().getName()));
-        manageMenu();
     }
 
-    private void manageMenu() {
+    private Displayable getNextDisplay() {
         Scanner scanner = new Scanner(System.in);
         String menu = scanner.nextLine();
         AdminOptions menuOption = AdminOptions.fromCode(menu);
 
         switch (menuOption) {
             case AIRCRAFTREGISTER:
-                System.out.println("aircraft");
-                break;
+                registerAircraftDisplayScreen.setBackMenu(this);
+                return registerAircraftDisplayScreen;
             case ADD_DESTINATION:
                 System.out.println("add destination");
                 break;
@@ -40,11 +41,12 @@ public class AdminMenuDisplay implements Displayable{
                 System.out.println("error");
                 break;
         }
+        return null;
     }
 
     @Override
     public Displayable proceedToNextDisplay() {
         display();
-        return null;
+        return getNextDisplay();
     }
 }
