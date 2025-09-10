ae512ee7ede598520128081781432ba1212e10dd
MITRAIS\Rahmawati_SA294 <rahmawatisitti.azizah@mitrais.com>
2025-09-10 13:47:03 +0700

    modify the wording for enter destination name

--- src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
+++ src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
@@ -7,7 +7,7 @@ public class MenuText {
     private static final String ADMIN_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Aircraft Management\n2.Destination Management\n3.Route Creation\n4.System Operation";
     private static final String PASSENGER_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Book Flight\n2.My Bookings";
     public static final String ADD_DESTINATION_HEADER = "===== ADD DESTINATION =====";
-    public static final String ENTER_DESTINATION_NAME = "Enter destination name";
+    public static final String ENTER_DESTINATION_NAME = "Enter destination name: ";
     private static final String DESTINATION_SUCCESSFULLY_ADDED = "%s added as a destination!";
 
     public static String getAdminMenuDisplayText(String name) {
