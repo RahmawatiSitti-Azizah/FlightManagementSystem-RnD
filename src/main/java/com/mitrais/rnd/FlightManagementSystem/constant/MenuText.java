e1c4628363bef90566dfbc90c792b45d31c98173
destanti-mitrais <destanti.fatwakhyuni@mitrais.com>
2025-09-10 16:21:50 +0700

    change wording of menu aircraft and destionation registration

--- src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
+++ src/main/java/com/mitrais/rnd/FlightManagementSystem/constant/MenuText.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MenuText {
-    private static final String ADMIN_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Aircraft Management\n2.Destination Management\n3.Route Creation\n4.System Operation";
+    private static final String ADMIN_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Register Aircraft\n2.Register Destination \n3.Create Route\n4.System Operation";
     private static final String PASSENGER_MENU_DISPLAY = "Welcome, %s!\nPlease select menu\n1.Book Flight\n2.My Bookings";
     public static final String ADD_DESTINATION_HEADER = "===== ADD DESTINATION =====";
     public static final String ENTER_DESTINATION_NAME = "Enter destination name: ";
